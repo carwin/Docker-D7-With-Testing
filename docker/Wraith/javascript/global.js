b1044f4bb1d8a8da1636f407ea91b02173eab4fd
Isaac Battaglia <izbatt@gmail.com>
2017-11-28 14:03:47 -0600

    Add files via upload

---
+++ docker/Wraith/javascript/global.js
@@ -0,0 +1,8 @@
+//document.msHidden = false;
+//document.msvisibilitychange = new Event('msvisibilitychange');
+//document.visibilityState = "visible"
+
+//module.exports = function (phantom, ready) {
+//    // make Wraith wait a bit longer before taking the screenshot
+//    setTimeout(ready, 3000); // you MUST call the ready() callback for Wraith to continue
+//}
